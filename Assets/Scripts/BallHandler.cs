4ffe69c49b80cab9185fb797a98a1161fdef5382
wsarsou <walaahsarsour@gmail.com>
2023-02-26 18:54:12 -0500

    added rigidbody, touchresponsiveness, & worldposition

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -5,9 +5,28 @@ using UnityEngine.InputSystem;
 
 public class BallHandler : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody2D currentBallRigidbody;
+    private Camera mainCamera;
+
     // Start is called before the first frame update
-    void Start() { }
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
 
     // Update is called once per frame
-    void Update() { }
+    void Update()
+    {
+        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            return;
+        }
+
+        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+
+        Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+
+        currentBallRigidbody.position = worldPosition;
+    }
 }
